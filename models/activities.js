9af5fd647ef67a324d48f7b41022f2fc653dbe22
casey <cassandra.d.mclellan@gmail.com>
2022-02-22 11:23:47 +0000

    FEAT: type query parameter added to filter data from GET /requests

--- models/activities.js
+++ models/activities.js
@@ -1,8 +1,12 @@
 import db from "../db/connection.js";
 
-export async function getAllActivities() {
+export async function getAllActivities(location, type, date) {
   const result = await db.query(
-    `SELECT * FROM activities LEFT JOIN users on users.user_id=activities.organiser_id LEFT JOIN locations on locations.location_id=activities.location_id`
+    `SELECT * FROM activities 
+    LEFT JOIN users on users.user_id=activities.organiser_id 
+    LEFT JOIN locations on locations.location_id=activities.location_id 
+    WHERE activities.type = $1;`,
+    [type]
   );
   return result.rows;
 }
--- routes/activities.js
+++ routes/activities.js
@@ -5,7 +5,12 @@ const router = express.Router();
 
 /* GET activities listing. */
 router.get("/", async function (req, res, next) {
-  const activties = await getAllActivities();
+  const location = req.query.location;
+  const date = req.query.date;
+  const type = req.query.type;
+  console.log(location, date, type);
+
+  const activties = await getAllActivities(location, type, date);
 
   res.json({
     success: true,
