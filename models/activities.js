f27a1a951f3af4bd9e82c591ef71c4121fd62bd2
casey <cassandra.d.mclellan@gmail.com>
2022-02-22 11:59:42 +0000

    FEAT: db query can be filtered by activity type, location, and date timestamp

--- models/activities.js
+++ models/activities.js
@@ -5,8 +5,10 @@ export async function getAllActivities(location, type, date) {
     `SELECT * FROM activities 
     LEFT JOIN users on users.user_id=activities.organiser_id 
     LEFT JOIN locations on locations.location_id=activities.location_id 
-    WHERE activities.type = $1;`,
-    [type]
+    WHERE ($1::text is null OR $1::text = activities.type)
+    AND ($2::text is null OR $2::text = locations.location_name)
+    AND ($3::timestamp is null OR $3::timestamp = activities.date_time);`,
+    [type, location, date]
   );
   return result.rows;
 }
