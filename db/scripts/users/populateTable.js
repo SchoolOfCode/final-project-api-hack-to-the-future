764c088b58526026398a5aab2d850122e64a0169
Kieran Megahey <kieranmegahey2@gmail.com>
2022-02-23 21:31:53 +1100

    Added more user data & changed participants table to not allow additonal rows when user_id and activit_id already exist together

--- db/scripts/users/populateTable.js
+++ db/scripts/users/populateTable.js
@@ -1,10 +1,38 @@
 import db from "../../connection.js";
 
-const response = await db.query(
-  `INSERT INTO users (user_id, user_name, email) VALUES ($1, $2, $3);`,
-  ["1", "Paddington Bear", "paddington@bear.com"]
-);
+const data = [
+  {
+    user_id: "1",
+    user_name: "Paddington Bear",
+    email: "paddington@bear.com",
+  },
+  {
+    user_id: "2",
+    user_name: "Winnie the Pooh",
+    email: "Winnie@bear.com",
+  },
+  {
+    user_id: "3",
+    user_name: "Baloo",
+    email: "Baloo@bear.com",
+  },
+  {
+    user_id: "4",
+    user_name: "Ruben",
+    email: "Ruben@bear.com",
+  },
+];
+async function populateUsersTable() {
+  for (let i = 0; i < data.length; i++) {
+    const response = await db.query(
+      `INSERT INTO users (user_id, user_name, email) VALUES ($1, $2, $3);`,
+      [data[i].user_id, data[i].user_name, data[i].email]
+    );
 
-console.log(response);
+    console.log(response);
+  }
+}
+
+await populateUsersTable();
 
 db.end();
