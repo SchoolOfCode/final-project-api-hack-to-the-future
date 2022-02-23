fa26a0168eb603838e8b14ecd82ce22be61eb785
Ruben Gonzalez <rubengon.uk@gmail.com>
2022-02-23 10:53:48 +0000

    change On Conflict to Update in participants models

--- models/participants.js
+++ models/participants.js
@@ -4,7 +4,7 @@ export async function upsertPartcipantsRow(userId, activityId, role) {
   const sqlString = `INSERT INTO participants 
     (user_id ,activity_id, participant_role)
     VALUES ($1, $2, $3) 
-    ON CONFLICT DO NOTHING RETURNING *;`;
+    ON CONFLICT (user_id, activity_id) DO UPDATE SET participant_role = $3 RETURNING *;`;
   const result = await db.query(sqlString, [userId, activityId, role]);
   return result.rows;
 }
