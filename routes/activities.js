51304440dae28e4635cb1fc67c7aa27c3ababeb3
Maria-C1 <mariacrawford789@gmail.com>
2022-02-24 10:36:01 +0000

    post new activity route

--- routes/activities.js
+++ routes/activities.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { getAllActivities } from "../models/activities.js";
+import { getAllActivities, createNewActivity } from "../models/activities.js";
+ 
 
 const router = express.Router();
 
@@ -10,11 +11,33 @@ router.get("/", async function (req, res, next) {
   const type = req.query.type;
   console.log(location, date, type);
 
-  const activties = await getAllActivities(location, type, date);
+  const activities = await getAllActivities(location, type, date);
 
   res.json({
     success: true,
     payload: activties,
+  });
+});
+
+// POST activity
+router.post("/", async function (req, res, next) {
+  const organiser_id = req.body.organiser_id;
+  const location_id = req.body.location_id;
+  const max_attendees = req.body.max_attendees;
+  const date_time = req.body.date_time;
+  const description = req.body.description;
+  const type = req.body.type;
+  const newActivity = await createNewActivity(
+    organiser_id,
+    location_id,
+    max_attendees,
+    date_time,
+    description,
+    type
+  );
+  res.json({
+    success: true,
+    payload: newActivity,
   });
 });
 
