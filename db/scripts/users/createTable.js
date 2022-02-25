97e8b3d0aa9b88d74a76e7249e1abc0dcc16989a
casey <cassandra.d.mclellan@gmail.com>
2022-02-25 09:34:59 +0000

    FIX: removed locations table from database schema

--- db/scripts/users/createTable.js
+++ db/scripts/users/createTable.js
@@ -1,7 +1,7 @@
 import db from "../../connection.js";
 
 const response = await db.query(
-  `CREATE TABLE IF NOT EXISTS users (user_id TEXT PRIMARY KEY, user_name TEXT NOT NULL, email text NOT NULL);`
+  `CREATE TABLE IF NOT EXISTS users (user_id TEXT PRIMARY KEY, user_name TEXT NOT NULL, email text);`
 );
 
 console.log(response);
