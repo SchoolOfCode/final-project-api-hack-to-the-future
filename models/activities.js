7eb99a479821e078bcc9c19a4000ac7525fa7b95
casey <cassandra.d.mclellan@gmail.com>
2022-02-25 09:44:59 +0000

    FIX: removed locations table join in getAllActivities model

--- models/activities.js
+++ models/activities.js
@@ -4,9 +4,8 @@ export async function getAllActivities(location, type, date) {
   const result = await db.query(
     `SELECT * FROM activities 
     LEFT JOIN users on users.user_id=activities.organiser_id 
-    LEFT JOIN locations on locations.location_id=activities.location_id 
     WHERE ($1::text is null OR $1::text = activities.type)
-    AND ($2::text is null OR $2::text = locations.location_name)
+    AND ($2::text is null OR $2::text = activities.location_name)
     AND ($3::timestamp is null OR $3::timestamp = activities.date_time);`,
     [type, location, date]
   );
