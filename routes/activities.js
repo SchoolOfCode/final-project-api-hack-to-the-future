1faa99ec5ca1dd3fd3f1a9349de474c5dd73f392
casey <cassandra.d.mclellan@gmail.com>
2022-03-01 10:30:18 +0000

    FIX: added user_id auth header to GET /activities

--- routes/activities.js
+++ routes/activities.js
@@ -5,20 +5,24 @@ const router = express.Router();
 
 /* GET activities listing. */
 router.get("/", async function (req, res, next) {
-  try {
-    const location = req.query.location;
-    const date = req.query.date;
-    const type = req.query.type;
-    console.log(location, date, type);
+  const user_id = req.headers.authorization;
+  if (user_id) {
+    try {
+      const location = req.query.location;
+      const date = req.query.date;
+      const type = req.query.type;
 
-    const activities = await getAllActivities(location, type, date);
+      const activities = await getAllActivities(location, type, date);
 
-    res.json({
-      success: true,
-      payload: activities,
-    });
-  } catch (error) {
-    next(error);
+      res.json({
+        success: true,
+        payload: activities,
+      });
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    res.json({ success: false, message: "Missing authorization header" });
   }
 });
 
