9983e85aa9c338d7e9142e489d2396ad539932a9
casey <cassandra.d.mclellan@gmail.com>
2022-03-01 10:42:25 +0000

    FIX: added user_id auth header to GET /users

--- routes/users.js
+++ routes/users.js
@@ -1,40 +1,45 @@
-import express from 'express';
-import { getAllUsers, createUser } from '../models/users.js';
+import express from "express";
+import { getAllUsers, createUser } from "../models/users.js";
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', async function (req, res, next) {
+router.get("/", async function (req, res, next) {
+  const user_id = req.headers.authorization;
+  if (user_id) {
     try {
-        const users = await getAllUsers();
+      const users = await getAllUsers();
 
-        res.json({
-            success: true,
-            payload: users,
-        });
+      res.json({
+        success: true,
+        payload: users,
+      });
     } catch (error) {
-        next(error);
+      next(error);
     }
+  } else {
+    res.json({ success: false, message: "Missing authorization header" });
+  }
 });
 
 /* PUT - create a new user if one with this ID doesn't exist yet */
-router.put('/', async function (req, res, next) {
-    try {
-        const user_id = req.headers.authorization;
+router.put("/", async function (req, res, next) {
+  try {
+    const user_id = req.headers.authorization;
 
-        const user_name = req.body.user_name;
+    const user_name = req.body.user_name;
 
-        const email = req.body.email;
+    const email = req.body.email;
 
-        const newUser = await createUser(user_id, user_name, email);
+    const newUser = await createUser(user_id, user_name, email);
 
-        res.json({
-            success: true,
-            payload: newUser,
-        });
-    } catch (error) {
-        next(error);
-    }
+    res.json({
+      success: true,
+      payload: newUser,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default router;
