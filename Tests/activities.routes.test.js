052701d6da51b8d0a6cf1cb74dc45b481c71dc18
Maria-C1 <mariacrawford789@gmail.com>
2022-03-02 14:11:33 +0000

    get activities and filter activities tests done

--- Tests/activities.routes.test.js
+++ Tests/activities.routes.test.js
@@ -12,7 +12,6 @@ describe("GET /activities", function () {
         expect.objectContaining({
           activity_id: expect.any(Number),
           organiser_id: expect.any(String),
-          location_id: expect.any(Number),
           max_attendees: expect.any(Number),
           date_time: expect.any(String),
           description: expect.any(String),
@@ -21,50 +20,39 @@ describe("GET /activities", function () {
           user_name: expect.any(String),
           email: expect.any(String),
           location_name: expect.any(String),
-          lat_long: expect.any(String),
-          is_accessible: expect.any(Boolean),
         }),
       ]),
     };
-    const actual = await request(app).get("/activities");
+    const actual = await request(app)
+      .get("/activities")
+      .set("Authorization", "test_user_id");
     expect(actual.body).toStrictEqual(expectedBody);
     expect(actual.statusCode).toBe(200);
   });
-  // test 2 - checks that the array contains the correct shape of the activity data with correct types
-  //   test("gives us an object with success: true and payload: an array", async function () {
-  //     const expectedPayload = [
-  //       {
-  //         activity_id: expect.any(Number),
-  //         organiser_id: expect.any(String),
-  //         location_id: expect.any(Number),
-  //         max_attendees: expect.any(Number),
-  //         date_time: expect.any(String),
-  //         description: expect.any(String),
-  //         type: expect.any(String),
-  //         user_id: expect.any(String),
-  //         user_name: expect.any(String),
-  //         email: expect.any(String),
-  //         location_name: expect.any(String),
-  //         lat_long: expect.any(String),
-  //         is_accessible: expect.any(Boolean),
-  //       },
-  //     ];
-  //     const expectedBody = {
-  //       success: true,
-  //       payload: expectedPayload,
-  //     };
-  //     const actual = await request(app).get("/activities");
-  //     expect(actual.body).toStrictEqual(expectedBody);
-  //     expect(actual.statusCode).toBe(200);
-  //   });
-});
 
-// describe('arrayContaining', () => {
-//     const expected = ['Alice', 'Bob'];
-//     it('matches even if received contains additional elements', () => {
-//       expect(['Alice', 'Bob', 'Eve']).toEqual(expect.arrayContaining(expected));
-//     });
-//     it('does not match if received does not contain expected elements', () => {
-//       expect(['Bob', 'Eve']).not.toEqual(expect.arrayContaining(expected));
-//     });
-//   });
+  test("gives us back the activities that match the selected filters", async function () {
+    const expectedBody = {
+      success: true,
+      payload: [
+        {
+          activity_id: 1,
+          date_time: "2022-03-02T12:26:07.278Z",
+          description:
+            "All abilities welcome to a walk round Cannon Hill Park followed by coffee at the park cafe",
+          email: "paddington@bear.com",
+          location_name: "Cannon Hill Park, Birmingham",
+          max_attendees: 8,
+          organiser_id: "1",
+          type: "walk",
+          user_id: "1",
+          user_name: "Paddington Bear",
+        },
+      ],
+    };
+    const actual = await request(app)
+      .get("/activities?type=walk")
+      .set("Authorization", "test_user_id");
+    expect(actual.body).toStrictEqual(expectedBody);
+    expect(actual.statusCode).toBe(200);
+  });
+});
