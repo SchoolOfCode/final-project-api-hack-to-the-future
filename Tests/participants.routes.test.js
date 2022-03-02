caaf5b48e750c90e561664f65b4309b6a524c191
Maria-C1 <mariacrawford789@gmail.com>
2022-03-02 16:06:36 +0000

    get activities based on user's role test

--- Tests/participants.routes.test.js
+++ Tests/participants.routes.test.js
@@ -8,7 +8,7 @@ afterAll((done) => {
 });
 
 describe("PUT /participants", function () {
-  test("upserts a participants relationship between user and activity and returns the data", async function () {
+  test("upserts a participants relationship to interested between user and activity and returns the data", async function () {
     const user_id = "2";
     const activity_id = 1;
     const participant_role = "interested";
@@ -30,6 +30,62 @@ describe("PUT /participants", function () {
         activity_id: activity_id,
         participant_role: participant_role,
       });
+    expect(actual.body).toStrictEqual(expectedBody);
+    expect(actual.statusCode).toBe(200);
+  });
+
+  test("upserts a participants relationship to attending between user and activity and returns the data", async function () {
+    const user_id = "2";
+    const activity_id = 1;
+    const participant_role = "attending";
+    const expectedBody = {
+      success: true,
+      payload: [
+        {
+          participant_id: expect.any(Number),
+          user_id: user_id,
+          activity_id: activity_id,
+          participant_role: participant_role,
+        },
+      ],
+    };
+    const actual = await request(app)
+      .put("/participants")
+      .set("Authorization", user_id)
+      .send({
+        activity_id: activity_id,
+        participant_role: participant_role,
+      });
+    expect(actual.body).toStrictEqual(expectedBody);
+    expect(actual.statusCode).toBe(200);
+  });
+});
+
+describe("GET /participants", function () {
+  test("gets all activities based on a user's role", async function () {
+    const user_id = "2";
+    const activity_id = 1;
+    const participant_role = "attending";
+    const expectedBody = {
+      success: true,
+      payload: expect.arrayContaining([
+        expect.objectContaining({
+          participant_id: expect.any(Number),
+          user_id: user_id,
+          activity_id: activity_id,
+          participant_role: participant_role,
+          organiser_id: expect.any(String),
+          location_name: expect.any(String),
+          max_attendees: expect.any(Number),
+          date_time: expect.any(String),
+          description: expect.any(String),
+          type: expect.any(String),
+        }),
+      ]),
+    };
+    const actual = await request(app)
+      .get(`/participants/${participant_role}`)
+      .set("Authorization", user_id);
     expect(actual.body).toStrictEqual(expectedBody);
     expect(actual.statusCode).toBe(200);
   });
